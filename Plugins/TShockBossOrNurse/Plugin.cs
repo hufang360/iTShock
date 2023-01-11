42ae94f854628acc74fcfb81dfd2292d939d6318
hufang360 <hufang360@sina.com>
2023-01-11 12:42:18 +0800

    hfTopy1 和 BossOrNurse

---
+++ Plugins/TShockBossOrNurse/Plugin.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.IO;
+using System.Reflection;
+using System.Text;
+using Terraria;
+using TerrariaApi.Server;
+using TShockAPI;
+
+namespace Plugin
+{
+    [ApiVersion(2, 1)]
+    public class Plugin : TerrariaPlugin
+    {
+
+        public override string Description => "Boss战期间和护士对话，护士会消失";
+        public override string Name => "Boss还是护士";
+        public override string Author => "hufang360";
+        public override Version Version => Assembly.GetExecutingAssembly().GetName().Version;
+
+
+        public Plugin(Main game) : base(game)
+        {
+        }
+
+
+        public override void Initialize()
+        {
+            ServerApi.Hooks.NetGetData.Register(this, OnGetData, 1);
+        }
+
+        /// <summary>
+        /// 消息处理
+        /// </summary>
+        void OnGetData(GetDataEventArgs args)
+        {
+            switch (args.MsgID)
+            {
+                case PacketTypes.NpcTalk:
+                    using (MemoryStream ms = new(args.Msg.readBuffer, args.Index, args.Length))
+                    using (BinaryReader br = new(ms, Encoding.UTF8, true))
+                    {
+                        int playerID = br.ReadByte();
+                        int npcTalkTarget = br.ReadInt16();
+                        if (npcTalkTarget == -1)
+                            return;
+
+                        // 是否是护士
+                        NPC npc = Main.npc[npcTalkTarget];
+                        if (npc.netID != 18)
+                            return;
+
+                        // 是否是boss战期间
+                        bool flag = false;
+                        for (int i = 0; i < 200; i++)
+                        {
+                            if (!Main.npc[i].active || !Main.npc[i].boss)
+                            {
+                                continue;
+                            }
+
+                            // 四柱
+                            switch (Main.npc[i].type)
+                            {
+                                case 422:
+                                case 493:
+                                case 507:
+                                case 517:
+                                    continue;
+                            }
+                            flag = true;
+                            break;
+                        }
+                        if (flag)
+                        {
+                            ClearNPCByID(npc.netID);
+                            TSPlayer.All.SendInfoMessage($"[i:{3544}]{npc.TypeName}已离开！");
+                            args.Handled = true;
+                        }
+                    }
+                    break;
+            }
+        }
+        
+        /// <summary>
+        /// 通过npcid清理npc
+        /// </summary>
+        static int ClearNPCByID(int npcID)
+        {
+            int cleared = 0;
+            for (int i = 0; i < Main.maxNPCs; i++)
+            {
+                if (Main.npc[i].active && Main.npc[i].netID == npcID)
+                {
+                    Main.npc[i].active = false;
+                    Main.npc[i].type = 0;
+                    TSPlayer.All.SendData(PacketTypes.NpcUpdate, "", i);
+                    cleared++;
+                }
+            }
+            return cleared;
+        }
+
+        /// <summary>
+        /// 销毁
+        /// </summary>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                ServerApi.Hooks.NetGetData.Deregister(this, OnGetData);
+            }
+            base.Dispose(disposing);
+        }
+
+    }
+}
---
+++ Plugins/TShockhfToy1/Plugin.cs
@@ -0,0 +1,89 @@
+﻿using Microsoft.Xna.Framework;
+using System;
+using System.Reflection;
+using Terraria;
+using TerrariaApi.Server;
+using TShockAPI;
+
+namespace Plugin
+{
+    [ApiVersion(2, 1)]
+    public class Plugin : TerrariaPlugin
+    {
+
+        public override string Description => "";
+        public override string Name => "hfToy-射弹发射器";
+        public override string Author => "hufang360";
+        public override Version Version => Assembly.GetExecutingAssembly().GetName().Version;
+
+
+        public Plugin(Main game) : base(game)
+        {
+        }
+
+        /// <summary>
+        /// 初始时
+        /// </summary>
+        public override void Initialize()
+        {
+            Commands.ChatCommands.Add(new Command("hf.toy.projectile", PCommand, "pj", "射弹") { HelpText = "射弹发射器", AllowServer=false });
+        }
+
+        /// <summary>
+        /// 销毁时
+        /// </summary>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+            }
+            base.Dispose(disposing);
+        }
+
+        /// <summary>
+        /// 处理指令
+        /// </summary>
+        void PCommand(CommandArgs args)
+        {
+            TSPlayer op = args.Player;
+
+            if (args.Parameters.Count == 0)
+            {
+                op.SendErrorMessage("请输入射弹id，例如 /pj 1013");
+                return;
+            }
+
+            if (int.TryParse(args.Parameters[0], out int projID))
+            {
+                if (projID > 1021 || projID <= 0)
+                    projID = 1013;
+            }
+            else
+            {
+                projID = 1013;
+            }
+
+            int total = 40;
+            if (args.Parameters.Count > 1 && int.TryParse(args.Parameters[1], out total))
+            {
+                if (total >= 800 || total < 1)
+                    total = 40;
+            }
+
+            Vector2 pos;
+            Vector2 vel;
+            int pIndex;
+            for (int i = 0; i < total; i++)
+            {
+                // 1013 弹力巨石
+                // 1021 月亮巨石
+                pos = new(op.TPlayer.position.X + (60 - i * 3) * 16, op.TPlayer.position.Y - 16 * (Main.rand.Next(10, 50)));
+                vel = new(Main.rand.Next(-10, 10), Main.rand.Next(2, 20));
+                pIndex = Projectile.NewProjectile(Projectile.GetNoneSource(), pos, vel, projID, 0, 0f);
+                NetMessage.SendData(27, -1, -1, null, pIndex);
+            }
+        }
+
+
+    }
+}
